80f8797c502ffa0ef7c8955e1dd6ca1c43ccd9c1
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2015-12-13 18:36:30 +0530

    Add shortcut to move a tab left or right

--- background.js
+++ background.js
@@ -2,11 +2,24 @@
 	'use strict';
 	// Awesome Browser Shortcuts
 	chrome.commands.onCommand.addListener(function (command) {
+		var tabQuery = {active: true, currentWindow: true};
 		if (command === 'toggle-pin') {
 			// Get current tab in the active window
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.query(tabQuery, function(tabs) {
 				var currentTab = tabs[0];
 				chrome.tabs.update(currentTab.id, {'pinned': !currentTab.pinned});
+			});
+		}
+		else if (command === 'move-tab-left') {
+			chrome.tabs.query(tabQuery, function (tabs) {
+				var currentTab = tabs[0];
+				chrome.tabs.move(currentTab.id, {'index': currentTab.index - 1});
+			});
+		}
+		else if (command === 'move-tab-right') {
+			chrome.tabs.query(tabQuery, function (tabs) {
+				var currentTab = tabs[0];
+				chrome.tabs.move(currentTab.id, {'index': currentTab.index + 1});
 			});
 		}
 	});
