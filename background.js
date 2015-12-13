f8ed32b7850514392a7a9ea2532d50dc233b0efb
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2015-12-13 18:15:46 +0530

    Fix toggle-pin and clean code

--- background.js
+++ background.js
@@ -2,12 +2,11 @@
 	'use strict';
 	// Awesome Browser Shortcuts
 	chrome.commands.onCommand.addListener(function (command) {
-		console.log(command);
 		if (command === 'toggle-pin') {
 			// Get current tab in the active window
 			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 				var currentTab = tabs[0];
-				chrome.tabs.update(currentTab.id, {'pinned': currentTab.pinned});
+				chrome.tabs.update(currentTab.id, {'pinned': !currentTab.pinned});
 			});
 		}
 	});
